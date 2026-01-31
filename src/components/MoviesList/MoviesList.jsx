cecea9b2237fced2b51e1d485c4a2e9a054caf30
marcoduartestorm <marcosbottocomp@gmail.com>
2026-01-31 10:26:28 -0300

    Solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,16 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
+export const MoviesList = ({ movies, query }) => (
   <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
+    {movies.map(movie => {
+      const title = movie.title.toLowerCase();
+      const description = movie.description.toLowerCase();
+      const queryTrimed = query.trim().toLowerCase();
+
+      if (title.includes(queryTrimed) || description.includes(queryTrimed)) {
+        return (<MovieCard key={movie.imdbId} movie={movie} />);
+      }
+    })}
   </div>
 );
