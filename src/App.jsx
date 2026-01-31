8665e121adf3a8aa2f59b16c5053bd81fde4b23f
marcoduartestorm <marcosbottocomp@gmail.com>
2026-01-31 15:24:01 -0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import moviesFromServer from './api/movies.json';
 
 export const App = () => {
   const [query, setQuery] = useState('');
+  const [visibleMovies, setVisibleMovies] = useState(moviesFromServer);
 
   return (
     <div className="page">
@@ -24,15 +25,23 @@ export const App = () => {
                 placeholder="Type search word"
                 onChange={event => {
                   setQuery(event.target.value);
+                  const queryTrimed = query.trim().toLowerCase();
+                  query !== '' ?
+                  setVisibleMovies(
+                    moviesFromServer.filter(
+                      movie =>
+                        movie.title.toLowerCase().includes(queryTrimed) ||
+                        movie.description.toLowerCase().includes(queryTrimed),
+                    ),
+                  ) : null;
                 }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} query={query} />
+        <MoviesList movies={visibleMovies} />
       </div>
-
       <div className="sidebar">Sidebar goes here</div>
     </div>
   );
--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,18 +1,10 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies, query }) => (
+export const MoviesList = ({ movies }) => (
   <div className="movies">
-    {movies.map(movie => {
-      const title = movie.title.toLowerCase();
-      const description = movie.description.toLowerCase();
-      const queryTrimed = query.trim().toLowerCase();
-
-      if (title.includes(queryTrimed) || description.includes(queryTrimed)) {
-        return <MovieCard key={movie.imdbId} movie={movie} />;
-      }
-
-      return null;
-    })}
+    {movies.map(movie => (
+      <MovieCard key={movie.imdbId} movie={movie} />
+    ))}
   </div>
 );
